d0087a2729f804a399381c89503ad55d8f816e3f
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2025-07-04 09:09:16 +0000

    2025/07/04_学習内容1(一時プッシュ)_=>[36.]

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,17 +1,15 @@
 import { getDetailArticle } from '@/blogAPI';
-import { get } from 'http'
 import Image from 'next/image'
 import React from 'react'
 
 const Article = async ({params}: {params: {id: string}}) => {
     
     const detailArticle = await getDetailArticle(params.id);
-    console.log(detailArticle);
 
     return (
     <div className='max-w-3xl mx-auto p-5'>
         <Image
-            src="https://picsum.photos/1280/300?sig=1"
+            src={`https://picsum.photos/1280/300?sig=${detailArticle.id}`}
             alt=""
             width={1280}
             height={300}
